0979ac708eebac159c05e146b117b4cf5ff6f13a
almasike <almasike000@gmail.com>
2023-07-21 10:35:36 -0700

    supper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to prirint
+ *
+ * Return:On succes 1.
+ * On error,-1 is returned,and ermo is set appropriately
+ */
+int _putcher(char)
+{
+	returen(write(1,&c,1));
+}
